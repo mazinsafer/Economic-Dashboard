59322df1aebdab6412d803af94c9111306b52dd7
mazinsafer <147009870+mazinsafer@users.noreply.github.com>
2024-10-02 00:00:37 -0500

    Update ecoproj.py

--- ecoproj.py
+++ ecoproj.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-# Title of your Streamlit app
-st.title("Power BI Dashboard in Streamlit")
 
-# Embed the Power BI report using an iframe
+st.title("U.S. Economic Dashboard")
+
 st.markdown(
     """
     <iframe width="100%" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiOWQxMDlkNDItNGE5MC00YmM2LTgwZDQtYWZkMjIwZTdmOTFhIiwidCI6IjE3MGJiYWJkLWEyZjAtNGM5MC1hZDRiLTBlOGYwZjBjNDI1OSIsImMiOjN9" frameborder="0" allowFullScreen="true"></iframe>
